8aab833c88ccbbe6cc9a07bb9e08ec4418d68acb
Alan Isaac <136749111+Dev-Alan-Isaac@users.noreply.github.com>
2024-07-12 15:42:43 -0600

    ...

--- Project #Filter/obj/Debug/net8.0-windows10.0.22621.0/Project #Filter.AssemblyInfo.cs
+++ Project #Filter/obj/Debug/net8.0-windows10.0.22621.0/Project #Filter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project #Filter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c22fd35e08be525695fb2180cb778373c760881")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51a4b2154049ae287a996fb36412a07702abf4c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project #Filter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project #Filter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
