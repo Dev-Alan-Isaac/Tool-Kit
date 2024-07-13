9eb40073afbe5713e26e460cc3ba07b71091044f
Alan <136749111+Dev-Alan-Isaac@users.noreply.github.com>
2024-07-13 01:19:35 -0600

    ...

--- Project #Filter/Opt_Transform.Designer.cs
+++ Project #Filter/Opt_Transform.Designer.cs
@@ -69,7 +69,7 @@
             label1.ForeColor = Color.White;
             label1.Location = new Point(3, 0);
             label1.Name = "label1";
-            label1.Size = new Size(89, 41);
+            label1.Size = new Size(71, 32);
             label1.TabIndex = 17;
             label1.Text = "Path:";
             // 
@@ -77,18 +77,20 @@
             // 
             panel_R.BackColor = Color.FromArgb(0, 64, 64);
             panel_R.Dock = DockStyle.Right;
-            panel_R.Location = new Point(762, 43);
+            panel_R.Location = new Point(667, 32);
+            panel_R.Margin = new Padding(3, 2, 3, 2);
             panel_R.Name = "panel_R";
-            panel_R.Size = new Size(37, 454);
+            panel_R.Size = new Size(32, 341);
             panel_R.TabIndex = 26;
             // 
             // panel_B
             // 
             panel_B.BackColor = Color.FromArgb(0, 64, 64);
             panel_B.Dock = DockStyle.Bottom;
-            panel_B.Location = new Point(0, 497);
+            panel_B.Location = new Point(0, 373);
+            panel_B.Margin = new Padding(3, 2, 3, 2);
             panel_B.Name = "panel_B";
-            panel_B.Size = new Size(799, 43);
+            panel_B.Size = new Size(699, 32);
             panel_B.TabIndex = 24;
             // 
             // panel_T
@@ -97,8 +99,9 @@
             panel_T.Controls.Add(button_Path);
             panel_T.Dock = DockStyle.Top;
             panel_T.Location = new Point(0, 0);
+            panel_T.Margin = new Padding(3, 2, 3, 2);
             panel_T.Name = "panel_T";
-            panel_T.Size = new Size(799, 43);
+            panel_T.Size = new Size(699, 32);
             panel_T.TabIndex = 23;
             // 
             // button_Path
@@ -109,9 +112,10 @@
             button_Path.BackgroundImageLayout = ImageLayout.Zoom;
             button_Path.FlatAppearance.BorderSize = 0;
             button_Path.FlatStyle = FlatStyle.Flat;
-            button_Path.Location = new Point(726, 5);
+            button_Path.Location = new Point(635, 4);
+            button_Path.Margin = new Padding(3, 2, 3, 2);
             button_Path.Name = "button_Path";
-            button_Path.Size = new Size(29, 33);
+            button_Path.Size = new Size(25, 25);
             button_Path.TabIndex = 0;
             button_Path.UseVisualStyleBackColor = false;
             button_Path.Click += button_Path_Click;
@@ -123,9 +127,10 @@
             panel_button.Controls.Add(checkBox_Delete);
             panel_button.Controls.Add(button_Convert);
             panel_button.Dock = DockStyle.Bottom;
-            panel_button.Location = new Point(0, 412);
+            panel_button.Location = new Point(0, 309);
+            panel_button.Margin = new Padding(3, 2, 3, 2);
             panel_button.Name = "panel_button";
-            panel_button.Size = new Size(762, 85);
+            panel_button.Size = new Size(667, 64);
             panel_button.TabIndex = 39;
             // 
             // label2
@@ -134,18 +139,19 @@
             label2.BackColor = Color.FromArgb(64, 64, 64);
             label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
             label2.ForeColor = Color.White;
-            label2.Location = new Point(3, 13);
+            label2.Location = new Point(3, 10);
             label2.Name = "label2";
-            label2.Size = new Size(367, 23);
+            label2.Size = new Size(307, 19);
             label2.TabIndex = 43;
             label2.Text = "Would you like to delete unused directories?";
             // 
             // progressBar_Time
             // 
             progressBar_Time.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            progressBar_Time.Location = new Point(3, 51);
+            progressBar_Time.Location = new Point(3, 38);
+            progressBar_Time.Margin = new Padding(3, 2, 3, 2);
             progressBar_Time.Name = "progressBar_Time";
-            progressBar_Time.Size = new Size(753, 29);
+            progressBar_Time.Size = new Size(659, 22);
             progressBar_Time.Step = 1;
             progressBar_Time.TabIndex = 42;
             // 
@@ -154,9 +160,10 @@
             checkBox_Delete.AutoSize = true;
             checkBox_Delete.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
             checkBox_Delete.ForeColor = Color.White;
-            checkBox_Delete.Location = new Point(382, 17);
+            checkBox_Delete.Location = new Point(334, 13);
+            checkBox_Delete.Margin = new Padding(3, 2, 3, 2);
             checkBox_Delete.Name = "checkBox_Delete";
-            checkBox_Delete.Size = new Size(18, 17);
+            checkBox_Delete.Size = new Size(15, 14);
             checkBox_Delete.TabIndex = 44;
             checkBox_Delete.UseVisualStyleBackColor = true;
             // 
@@ -169,9 +176,10 @@
             button_Convert.FlatStyle = FlatStyle.Flat;
             button_Convert.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold);
             button_Convert.ForeColor = Color.White;
-            button_Convert.Location = new Point(490, 8);
+            button_Convert.Location = new Point(429, 6);
+            button_Convert.Margin = new Padding(3, 2, 3, 2);
             button_Convert.Name = "button_Convert";
-            button_Convert.Size = new Size(266, 35);
+            button_Convert.Size = new Size(233, 26);
             button_Convert.TabIndex = 45;
             button_Convert.Text = "Convert";
             button_Convert.UseVisualStyleBackColor = false;
@@ -182,9 +190,10 @@
             panel_Path.Controls.Add(textBox_Path);
             panel_Path.Controls.Add(label1);
             panel_Path.Dock = DockStyle.Top;
-            panel_Path.Location = new Point(0, 43);
+            panel_Path.Location = new Point(0, 32);
+            panel_Path.Margin = new Padding(3, 2, 3, 2);
             panel_Path.Name = "panel_Path";
-            panel_Path.Size = new Size(762, 96);
+            panel_Path.Size = new Size(667, 72);
             panel_Path.TabIndex = 40;
             // 
             // textBox_Path
@@ -194,10 +203,11 @@
             textBox_Path.BorderStyle = BorderStyle.FixedSingle;
             textBox_Path.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
             textBox_Path.ForeColor = Color.White;
-            textBox_Path.Location = new Point(98, 5);
+            textBox_Path.Location = new Point(86, 4);
+            textBox_Path.Margin = new Padding(3, 2, 3, 2);
             textBox_Path.Name = "textBox_Path";
             textBox_Path.ReadOnly = true;
-            textBox_Path.Size = new Size(653, 39);
+            textBox_Path.Size = new Size(572, 33);
             textBox_Path.TabIndex = 26;
             // 
             // panel_Middle
@@ -205,9 +215,10 @@
             panel_Middle.BackColor = Color.FromArgb(64, 64, 64);
             panel_Middle.Controls.Add(panel_Action);
             panel_Middle.Dock = DockStyle.Fill;
-            panel_Middle.Location = new Point(0, 139);
+            panel_Middle.Location = new Point(0, 104);
+            panel_Middle.Margin = new Padding(3, 2, 3, 2);
             panel_Middle.Name = "panel_Middle";
-            panel_Middle.Size = new Size(762, 273);
+            panel_Middle.Size = new Size(667, 205);
             panel_Middle.TabIndex = 41;
             // 
             // panel_Action
@@ -222,9 +233,8 @@
             panel_Action.Controls.Add(label_Warning);
             panel_Action.Dock = DockStyle.Fill;
             panel_Action.Location = new Point(0, 0);
-            panel_Action.Margin = new Padding(3, 4, 3, 4);
             panel_Action.Name = "panel_Action";
-            panel_Action.Size = new Size(762, 273);
+            panel_Action.Size = new Size(667, 205);
             panel_Action.TabIndex = 41;
             // 
             // radioButton_Size
@@ -233,9 +243,10 @@
             radioButton_Size.Enabled = false;
             radioButton_Size.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
             radioButton_Size.ForeColor = Color.White;
-            radioButton_Size.Location = new Point(256, 198);
+            radioButton_Size.Location = new Point(224, 148);
+            radioButton_Size.Margin = new Padding(3, 2, 3, 2);
             radioButton_Size.Name = "radioButton_Size";
-            radioButton_Size.Size = new Size(144, 32);
+            radioButton_Size.Size = new Size(115, 25);
             radioButton_Size.TabIndex = 46;
             radioButton_Size.TabStop = true;
             radioButton_Size.Text = "Sort by size";
@@ -247,9 +258,10 @@
             radioButton_Date.Enabled = false;
             radioButton_Date.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
             radioButton_Date.ForeColor = Color.White;
-            radioButton_Date.Location = new Point(20, 198);
+            radioButton_Date.Location = new Point(18, 148);
+            radioButton_Date.Margin = new Padding(3, 2, 3, 2);
             radioButton_Date.Name = "radioButton_Date";
-            radioButton_Date.Size = new Size(150, 32);
+            radioButton_Date.Size = new Size(120, 25);
             radioButton_Date.TabIndex = 45;
             radioButton_Date.TabStop = true;
             radioButton_Date.Text = "Sort by date";
@@ -261,9 +273,10 @@
             radioButton_Name.Enabled = false;
             radioButton_Name.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
             radioButton_Name.ForeColor = Color.White;
-            radioButton_Name.Location = new Point(20, 131);
+            radioButton_Name.Location = new Point(18, 98);
+            radioButton_Name.Margin = new Padding(3, 2, 3, 2);
             radioButton_Name.Name = "radioButton_Name";
-            radioButton_Name.Size = new Size(160, 32);
+            radioButton_Name.Size = new Size(129, 25);
             radioButton_Name.TabIndex = 44;
             radioButton_Name.TabStop = true;
             radioButton_Name.Text = "Sort by name";
@@ -275,9 +288,9 @@
             label3.BackColor = Color.FromArgb(64, 64, 64);
             label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             label3.ForeColor = Color.White;
-            label3.Location = new Point(3, 7);
+            label3.Location = new Point(3, 5);
             label3.Name = "label3";
-            label3.Size = new Size(130, 41);
+            label3.Size = new Size(104, 32);
             label3.TabIndex = 39;
             label3.Text = "Options";
             // 
@@ -290,10 +303,11 @@
             comboBox_Select.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold);
             comboBox_Select.ForeColor = Color.White;
             comboBox_Select.FormattingEnabled = true;
-            comboBox_Select.Items.AddRange(new object[] { "IMAGE To PDF [TITLE]", "IMAGE To PDF [NO TITLE]", "IMAGE To ICO", "IMAGE To WEBP", "IMAGE To BMP", "VIDEO To GIF", "VIDEO To WEBM", "VIDEO To AVI", "VIDEO To AUDIO", "AUDIO To WAV" });
-            comboBox_Select.Location = new Point(139, 9);
+            comboBox_Select.Items.AddRange(new object[] { "IMAGE To PDF [TITLE]", "IMAGE To PDF [NO TITLE]", "IMAGE To ICO", "IMAGE To WEBP", "IMAGE To BMP", "VIDEO To GIF", "VIDEO To WEBM", "VIDEO To AVI", "VIDEO To AUDIO", "AUDIO To WAV", "DOC To PDF" });
+            comboBox_Select.Location = new Point(122, 7);
+            comboBox_Select.Margin = new Padding(3, 2, 3, 2);
             comboBox_Select.Name = "comboBox_Select";
-            comboBox_Select.Size = new Size(333, 36);
+            comboBox_Select.Size = new Size(292, 29);
             comboBox_Select.TabIndex = 40;
             comboBox_Select.SelectedIndexChanged += comboBox_Select_SelectedIndexChanged;
             // 
@@ -303,10 +317,9 @@
             panel1.Controls.Add(label4);
             panel1.Controls.Add(listBox_File);
             panel1.Dock = DockStyle.Right;
-            panel1.Location = new Point(480, 0);
-            panel1.Margin = new Padding(3, 4, 3, 4);
+            panel1.Location = new Point(420, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(282, 273);
+            panel1.Size = new Size(247, 205);
             panel1.TabIndex = 43;
             // 
             // label_Count
@@ -314,9 +327,9 @@
             label_Count.AutoSize = true;
             label_Count.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label_Count.ForeColor = Color.White;
-            label_Count.Location = new Point(165, 7);
+            label_Count.Location = new Point(144, 5);
             label_Count.Name = "label_Count";
-            label_Count.Size = new Size(84, 28);
+            label_Count.Size = new Size(67, 21);
             label_Count.TabIndex = 5;
             label_Count.Text = "File List";
             // 
@@ -325,9 +338,9 @@
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label4.ForeColor = Color.White;
-            label4.Location = new Point(3, 7);
+            label4.Location = new Point(3, 5);
             label4.Name = "label4";
-            label4.Size = new Size(162, 28);
+            label4.Size = new Size(120, 21);
             label4.TabIndex = 4;
             label4.Text = "File List           #";
             // 
@@ -335,10 +348,10 @@
             // 
             listBox_File.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             listBox_File.FormattingEnabled = true;
-            listBox_File.Location = new Point(10, 49);
-            listBox_File.Margin = new Padding(3, 4, 3, 4);
+            listBox_File.ItemHeight = 15;
+            listBox_File.Location = new Point(9, 37);
             listBox_File.Name = "listBox_File";
-            listBox_File.Size = new Size(266, 224);
+            listBox_File.Size = new Size(233, 169);
             listBox_File.TabIndex = 0;
             listBox_File.SelectedIndexChanged += listBox_File_SelectedIndexChanged;
             // 
@@ -347,9 +360,9 @@
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label6.ForeColor = Color.White;
-            label6.Location = new Point(3, 85);
+            label6.Location = new Point(3, 64);
             label6.Name = "label6";
-            label6.Size = new Size(100, 32);
+            label6.Size = new Size(78, 25);
             label6.TabIndex = 2;
             label6.Text = "Actions";
             // 
@@ -358,14 +371,14 @@
             label_Warning.AutoSize = true;
             label_Warning.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label_Warning.ForeColor = Color.FromArgb(255, 128, 0);
-            label_Warning.Location = new Point(3, 137);
+            label_Warning.Location = new Point(3, 103);
             label_Warning.Name = "label_Warning";
-            label_Warning.Size = new Size(0, 32);
+            label_Warning.Size = new Size(0, 25);
             label_Warning.TabIndex = 0;
             // 
             // Opt_Transform
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(64, 64, 64);
             Controls.Add(panel_Middle);
@@ -374,8 +387,9 @@
             Controls.Add(panel_R);
             Controls.Add(panel_B);
             Controls.Add(panel_T);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Opt_Transform";
-            Size = new Size(799, 540);
+            Size = new Size(699, 405);
             panel_T.ResumeLayout(false);
             panel_button.ResumeLayout(false);
             panel_button.PerformLayout();
