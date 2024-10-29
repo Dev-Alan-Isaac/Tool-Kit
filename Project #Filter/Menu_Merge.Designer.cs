6bf82f2ba56405a50e5a664e3ba79c93dc7be1e8
Alan Isaac <136749111+Dev-Alan-Isaac@users.noreply.github.com>
2024-10-29 17:04:58 -0600

    ...

--- Project #Filter/Menu_Merge.Designer.cs
+++ Project #Filter/Menu_Merge.Designer.cs
@@ -29,9 +29,183 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            panel4 = new Panel();
+            label1 = new Label();
+            panel3 = new Panel();
+            panel1 = new Panel();
+            bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(components);
+            button_Saved = new Button();
+            panel10 = new Panel();
+            panel2 = new Panel();
+            checkBox_Delete = new CheckBox();
+            checkBox_Subfolders = new CheckBox();
+            checkBox_Keep = new CheckBox();
+            panel5 = new Panel();
+            panel4.SuspendLayout();
+            panel10.SuspendLayout();
+            panel5.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(label1);
+            panel4.Dock = DockStyle.Top;
+            panel4.Location = new Point(0, 32);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(579, 44);
+            panel4.TabIndex = 70;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Dock = DockStyle.Fill;
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.Control;
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(316, 37);
+            label1.TabIndex = 0;
+            label1.Text = "ADDITIONAL OPTIONS:";
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(0, 64, 64);
+            panel3.Dock = DockStyle.Bottom;
+            panel3.Location = new Point(0, 482);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(579, 32);
+            panel3.TabIndex = 69;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(0, 64, 64);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(579, 32);
+            panel1.TabIndex = 67;
+            // 
+            // bunifuElipse1
+            // 
+            bunifuElipse1.ElipseRadius = 5;
+            bunifuElipse1.TargetControl = button_Saved;
+            // 
+            // button_Saved
+            // 
+            button_Saved.BackColor = Color.Teal;
+            button_Saved.BackgroundImage = Properties.Resources.Button_Icon_Save;
+            button_Saved.BackgroundImageLayout = ImageLayout.Zoom;
+            button_Saved.Dock = DockStyle.Fill;
+            button_Saved.FlatAppearance.BorderSize = 0;
+            button_Saved.FlatStyle = FlatStyle.Flat;
+            button_Saved.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold);
+            button_Saved.ForeColor = Color.White;
+            button_Saved.Location = new Point(0, 0);
+            button_Saved.Margin = new Padding(3, 2, 3, 2);
+            button_Saved.Name = "button_Saved";
+            button_Saved.Size = new Size(570, 43);
+            button_Saved.TabIndex = 52;
+            button_Saved.UseVisualStyleBackColor = false;
+            // 
+            // panel10
+            // 
+            panel10.Controls.Add(button_Saved);
+            panel10.Dock = DockStyle.Bottom;
+            panel10.Location = new Point(0, 351);
+            panel10.Name = "panel10";
+            panel10.Size = new Size(570, 43);
+            panel10.TabIndex = 57;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(0, 64, 64);
+            panel2.Dock = DockStyle.Right;
+            panel2.Location = new Point(579, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(32, 514);
+            panel2.TabIndex = 68;
+            // 
+            // checkBox_Delete
+            // 
+            checkBox_Delete.AutoSize = true;
+            checkBox_Delete.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold);
+            checkBox_Delete.ForeColor = SystemColors.Control;
+            checkBox_Delete.Location = new Point(3, 3);
+            checkBox_Delete.Name = "checkBox_Delete";
+            checkBox_Delete.Size = new Size(214, 29);
+            checkBox_Delete.TabIndex = 58;
+            checkBox_Delete.Text = "Delete Empty Folders";
+            checkBox_Delete.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_Subfolders
+            // 
+            checkBox_Subfolders.AutoSize = true;
+            checkBox_Subfolders.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold);
+            checkBox_Subfolders.ForeColor = SystemColors.Control;
+            checkBox_Subfolders.Location = new Point(3, 41);
+            checkBox_Subfolders.Name = "checkBox_Subfolders";
+            checkBox_Subfolders.Size = new Size(255, 29);
+            checkBox_Subfolders.TabIndex = 59;
+            checkBox_Subfolders.Text = "Process Files in Subfolders";
+            checkBox_Subfolders.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_Keep
+            // 
+            checkBox_Keep.AutoSize = true;
+            checkBox_Keep.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold);
+            checkBox_Keep.ForeColor = SystemColors.Control;
+            checkBox_Keep.Location = new Point(3, 79);
+            checkBox_Keep.Name = "checkBox_Keep";
+            checkBox_Keep.Size = new Size(215, 29);
+            checkBox_Keep.TabIndex = 61;
+            checkBox_Keep.Text = "Keep the Original File";
+            checkBox_Keep.UseVisualStyleBackColor = true;
+            // 
+            // panel5
+            // 
+            panel5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel5.Controls.Add(checkBox_Delete);
+            panel5.Controls.Add(checkBox_Subfolders);
+            panel5.Controls.Add(checkBox_Keep);
+            panel5.Controls.Add(panel10);
+            panel5.Location = new Point(3, 82);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(570, 394);
+            panel5.TabIndex = 71;
+            // 
+            // Menu_Merge
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(64, 64, 64);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel1);
+            Controls.Add(panel2);
+            Controls.Add(panel5);
+            Name = "Menu_Merge";
+            Size = new Size(611, 514);
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel10.ResumeLayout(false);
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel4;
+        private Label label1;
+        private Panel panel3;
+        private Panel panel1;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private Button button_Saved;
+        private Panel panel10;
+        private Panel panel2;
+        private CheckBox checkBox_Delete;
+        private CheckBox checkBox_Subfolders;
+        private CheckBox checkBox_Keep;
+        private Panel panel5;
     }
 }
