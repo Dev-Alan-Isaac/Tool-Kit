453bb2804d3a8d1527d600a0cdba8b1f71d25ec8
Alan Isaac <136749111+Dev-Alan-Isaac@users.noreply.github.com>
2024-12-04 17:16:05 -0600

    ....

--- Project #Filter/Option_Download.Designer.cs
+++ Project #Filter/Option_Download.Designer.cs
@@ -40,8 +40,20 @@
             panel_Right = new Panel();
             bunifuElipse3 = new Bunifu.Framework.UI.BunifuElipse(components);
             panel_Footer = new Panel();
+            panel2 = new Panel();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            panel_Header = new Panel();
+            panel1 = new Panel();
+            label1 = new Label();
+            textBox_Path = new TextBox();
+            label_Warning = new Label();
             panel_Top.SuspendLayout();
             panel_Footer.SuspendLayout();
+            panel2.SuspendLayout();
+            panel_Header.SuspendLayout();
             SuspendLayout();
             // 
             // bunifuElipse4
@@ -153,11 +165,116 @@
             panel_Footer.Size = new Size(667, 64);
             panel_Footer.TabIndex = 56;
             // 
+            // panel2
+            // 
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(label2);
+            panel2.Location = new Point(3, 86);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(658, 375);
+            panel2.TabIndex = 58;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(15, 205);
+            label5.Name = "label5";
+            label5.Size = new Size(38, 15);
+            label5.TabIndex = 3;
+            label5.Text = "label5";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(15, 75);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 2;
+            label4.Text = "label4";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(3, 172);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 1;
+            label3.Text = "label3";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(3, 44);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 0;
+            label2.Text = "label2";
+            // 
+            // panel_Header
+            // 
+            panel_Header.Controls.Add(panel1);
+            panel_Header.Controls.Add(label1);
+            panel_Header.Controls.Add(textBox_Path);
+            panel_Header.Controls.Add(label_Warning);
+            panel_Header.Dock = DockStyle.Top;
+            panel_Header.Location = new Point(0, 32);
+            panel_Header.Margin = new Padding(3, 2, 3, 2);
+            panel_Header.Name = "panel_Header";
+            panel_Header.Size = new Size(667, 49);
+            panel_Header.TabIndex = 50;
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(332, 49);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(329, 380);
+            panel1.TabIndex = 59;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(64, 64, 64);
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 32);
+            label1.TabIndex = 23;
+            label1.Text = "Path:";
+            // 
+            // textBox_Path
+            // 
+            textBox_Path.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox_Path.BackColor = Color.FromArgb(64, 64, 64);
+            textBox_Path.BorderStyle = BorderStyle.FixedSingle;
+            textBox_Path.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
+            textBox_Path.ForeColor = Color.White;
+            textBox_Path.Location = new Point(86, 4);
+            textBox_Path.Margin = new Padding(3, 2, 3, 2);
+            textBox_Path.Name = "textBox_Path";
+            textBox_Path.Size = new Size(572, 33);
+            textBox_Path.TabIndex = 25;
+            // 
+            // label_Warning
+            // 
+            label_Warning.Anchor = AnchorStyles.Top;
+            label_Warning.AutoSize = true;
+            label_Warning.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold);
+            label_Warning.ForeColor = Color.FromArgb(255, 128, 0);
+            label_Warning.Location = new Point(935, 43);
+            label_Warning.Name = "label_Warning";
+            label_Warning.Size = new Size(0, 21);
+            label_Warning.TabIndex = 29;
+            // 
             // Option_Download
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(64, 64, 64);
+            Controls.Add(panel_Header);
+            Controls.Add(panel2);
             Controls.Add(panel_Footer);
             Controls.Add(panel_Right);
             Controls.Add(panel_Top);
@@ -166,6 +283,10 @@
             Size = new Size(699, 562);
             panel_Top.ResumeLayout(false);
             panel_Footer.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel_Header.ResumeLayout(false);
+            panel_Header.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -182,5 +303,15 @@
         private Panel panel_Right;
         private Bunifu.Framework.UI.BunifuElipse bunifuElipse3;
         private Panel panel_Footer;
+        private Panel panel2;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Panel panel_Header;
+        private Panel panel1;
+        private Label label1;
+        private TextBox textBox_Path;
+        private Label label_Warning;
     }
 }
